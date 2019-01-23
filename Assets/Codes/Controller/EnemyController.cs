83bb6783a42820eb04b18d3029a14802cbffdcce
charlesdai <email@charles_dai@qq.com>
2019-01-22 19:29:52 -0600

    Add enemy spawner
    now we can generate enemy

---
+++ Assets/Codes/Controller/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public Rigidbody2D enemy;
+
+	private GameObject player;
+
+	private Vector2 movement;
+	
+	// Use this for initialization
+	void Start () {
+		player =  GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		movement = (player.transform.position - this.transform.position).normalized;
+		enemy.velocity = movement * moveSpeed;
+	}
+}
---
+++ Assets/Codes/Controller/EnemySpawnerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerController : MonoBehaviour {
+
+	public static bool isStop = false;
+	public int spawnInterval;
+	public GameObject enemy;
+	
+	private int frames;
+	private int count;
+	
+	// Use this for initialization
+	void Start () {
+		frames = Mathf.RoundToInt(1.0f / Time.fixedDeltaTime);
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		Debug.Log("fuck");
+		if (!isStop)
+			count++;
+		else
+			count = 0;
+		if (count > frames * spawnInterval)
+		{
+			count = 0;
+			InstantiateEnemy("normal");
+		}
+	}
+
+	private void InstantiateEnemy(string type)
+	{
+		if (type.Equals("normal"))
+			Instantiate(enemy, this.transform.position, this.transform.rotation);
+	}
+}
